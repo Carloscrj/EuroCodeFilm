ec435183c21fb5c6b0ad71d5e809e98b485d862c
Baljaudev <r.baljau@gmail.com>
2023-03-09 18:50:52 +0100

    Implementado Main, logo, imgBtn y colores

--- app/src/main/java/uem/dam/eurocodefilms/MainActivity.java
+++ app/src/main/java/uem/dam/eurocodefilms/MainActivity.java
@@ -1,14 +1,61 @@
 package uem.dam.eurocodefilms;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+import uem.dam.eurocodefilms.fragments.InicioFragment;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageButton btnCartelera;
+    ImageButton btnMapa;
+    ImageButton btnPerfil;
+    FragmentManager fm;
+    FragmentTransaction ft;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+
+        ft.add(R.id.flContenedor, new InicioFragment());
+        ft.commit();
+
+        btnCartelera = findViewById(R.id.btnCartelera);
+        btnMapa = findViewById(R.id.btnMapa);
+        btnPerfil = findViewById(R.id.btnPerfil);
+
+        btnCartelera.setOnClickListener(this);
+        btnMapa.setOnClickListener(this);
+        btnPerfil.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnCartelera) cargarCartelera();
+        else if (v.getId() == R.id.btnMapa) cargarMapa();
+        else if (v.getId() == R.id.btnPerfil) cargarPerfil();
+    }
+
+    private void cargarCartelera() {
+
+    }
+
+    private void cargarMapa() {
+
+    }
+
+    private void cargarPerfil() {
+
+    }
+
 }
---
+++ app/src/main/java/uem/dam/eurocodefilms/fragments/InicioFragment.java
@@ -0,0 +1,22 @@
+package uem.dam.eurocodefilms.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import uem.dam.eurocodefilms.R;
+
+public class InicioFragment extends Fragment {
+
+    public InicioFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_inicio, container, false);
+    }
+}
